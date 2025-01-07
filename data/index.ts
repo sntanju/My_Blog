fd846f6e2d3bd85debecf801fe546c33236b7b1b
Sanjida Nasrin Tanju <86721558+sntanju@users.noreply.github.com>
2025-01-07 22:43:29 +0600

    init

---
+++ data/index.ts
@@ -0,0 +1,94 @@
+export const navbarData = {
+  homeTitle: "Sanjida's Blog",
+}
+
+export const footerData = {
+  author: 'Sanjida Nasrin',
+  aboutAuthor:
+    'Hi! I am Sanjida, a Tech enthusiast, problem solver and software developer.',
+  authorInterest:
+    "I have a fair amount of knowledge of Javascript, Typescript, ReactJs, PHP and Laravel.",
+  aboutTheSite:
+    "This is a personal blog site built with Nuxt3, TailwindCSS, NuxtContent, Nuxt Icon. Currently it's deployed in Vercel.",
+}
+
+export const homePage = {
+  title: 'Welcome To My Blog Site',
+  description:
+    'Get Web Development, Javascript, Typescript, NodeJs, Vue, and Nuxt, Related Articles, Tips, Learning resources and more.',
+}
+
+export const blogsPage = {
+  title: 'All Blogs',
+  description: 'Here you will find all the blog posts I have written & published on this site.',
+}
+
+export const categoryPage = {
+  title: 'Categories',
+  description:
+    'Blow this category is generated from all the tags are mentioned in the different blog post',
+}
+
+export const aboutPage = {
+  title: 'Sanjida Nasrin',
+  description: 'Software Developer, Problem Solver, Web Enthusiast.',
+  aboutMe:
+    "I'm a passionate software developer who loves writing code and constantly learning new things. Though currently unemployed, I'm actively honing my skills and crafting projects to sharpen my expertise. Problem-solving is my superpower, and I'm always up for tackling challenges head-on!",
+}
+
+export const seoData = {
+  title: `Sanjida's Blog | Sanjidas Blog`,
+  ogTitle: `Let's learn Javascript, Typescript, Vue, Nuxt, & Problem Solving - Riyads Blog | Riyad's Blog`,
+  description: `Hi I am Sanjida. A Software Developer, with over Proper knowledge in software development. - Sanjidas Blog | Sanjida's Blog`,
+  twitterDescription: `Sanjida's Blog, where I play around with Nuxt, Vue, and more and showcase my blog, resources, etc - Sanjidas Blog | Sanjida's Blog`,
+  image:
+    'https://res.cloudinary.com/dmecmyphj/image/upload/v1673548905/nuxt-blog/cover_ntgs6u.webp',
+  mySite: 'https://blog-nurriyad.vercel.app',
+  twitterHandle: '@sntanju07',
+  mailAddress: 'sntanju07@gmail.com',
+}
+
+export const siteMetaData = [
+  {
+    name: 'description',
+    content: seoData.description,
+  },
+  // Test on: https://developers.facebook.com/tools/debug/ or https://socialsharepreview.com/
+  { property: 'og:site_name', content: seoData.mySite },
+  { property: 'og:type', content: 'website' },
+  {
+    property: 'og:url',
+    content: seoData.mySite,
+  },
+  {
+    property: 'og:title',
+    content: seoData.ogTitle,
+  },
+  {
+    property: 'og:description',
+    content: seoData.description,
+  },
+  {
+    property: 'og:image',
+    content: seoData.image,
+  },
+  // Test on: https://cards-dev.twitter.com/validator or https://socialsharepreview.com/
+  { name: 'twitter:site', content: seoData.twitterHandle },
+  { name: 'twitter:card', content: 'summary_large_image' },
+  {
+    name: 'twitter:url',
+    content: seoData.mySite,
+  },
+  {
+    name: 'twitter:title',
+    content: seoData.ogTitle,
+  },
+  {
+    name: 'twitter:description',
+    content: seoData.twitterDescription,
+  },
+  {
+    name: 'twitter:image',
+    content: seoData.image,
+  },
+]
---
+++ nuxt.config.ts
@@ -0,0 +1,68 @@
+import { seoData } from './data'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-09-30',
+
+  modules: [
+    'nuxt-icon',
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
+    '@vueuse/nuxt',
+    '@nuxt/content',
+    'nuxt-og-image',
+    '@nuxtjs/robots',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/tailwindcss',
+    '@formkit/auto-animate',
+    '@stefanobartoletti/nuxt-social-share',
+  ],
+
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width,initial-scale=1',
+      title: seoData.title,
+      titleTemplate: `%s - ${seoData.title}`,
+    },
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' },
+  },
+
+  sitemap: {
+    strictNuxtContentPaths: true,
+  },
+
+  site: {
+    url: seoData.mySite,
+    identity: {
+      type: 'Person',
+    },
+    twitter: seoData.twitterHandle,
+  },
+
+  typescript: {
+    strict: true,
+  },
+
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ['/', '/rss.xml'],
+    },
+  },
+
+  colorMode: {
+    classSuffix: '',
+    preference: 'dark',
+    fallback: 'light',
+  },
+
+  content: {
+    highlight: {
+      theme: 'dracula',
+    },
+  },
+})
